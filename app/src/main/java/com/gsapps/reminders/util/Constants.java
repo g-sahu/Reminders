f980a2387b3ba42ab6d2048abe3b65f4e237b82a
Gaurav Sahu <contact.gauravsahu@gmail.com>
2019-03-26 22:17:39 +0530

    Changed Microsoft client ID

--- app/src/main/java/com/gsapps/reminders/util/Constants.java
+++ app/src/main/java/com/gsapps/reminders/util/Constants.java
@@ -3,7 +3,7 @@ package com.gsapps.reminders.util;
 public class Constants {
     // TODO: 24-03-2019 Store the client ids in a secure way
     public static final String GOOGLE_AUTH_CLIENT_ID = "903110263645-q7spnpsb0eof9c374s0pft1d6nsvcn63.apps.googleusercontent.com";
-    public static final String MS_AUTH_CLIENT_ID = "5ef767c0-524b-4759-8e06-9b330f77c050";
+    public static final String MS_AUTH_CLIENT_ID = "86773c8b-b482-474b-abf4-c7e2599611f8";
     public static final String CALENDAR_FRAGMENT = "CALENDAR_FRAGMENT";
     public static final String CONTACT_EVENTS_FRAGMENT = "CONTACT_EVENTS_FRAGMENT";
     public static final String MEETINGS_FRAGMENT = "MEETINGS_FRAGMENT";

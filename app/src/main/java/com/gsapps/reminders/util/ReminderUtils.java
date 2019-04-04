558130560d386e33f9c474ad10951029ef9f803f
Gaurav Sahu <contact.gauravsahu@gmail.com>
2019-04-04 22:01:07 +0530

    Added utility method for DateTimeTimeZone to Calendar conversion.

--- app/src/main/java/com/gsapps/reminders/util/ReminderUtils.java
+++ app/src/main/java/com/gsapps/reminders/util/ReminderUtils.java
@@ -9,9 +9,14 @@ import android.support.annotation.NonNull;
 import android.widget.Toast;
 import com.google.api.client.util.DateTime;
 import com.google.api.services.calendar.model.EventDateTime;
+import com.microsoft.graph.extensions.DateTimeTimeZone;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import static android.content.pm.PackageManager.PERMISSION_GRANTED;
@@ -67,4 +72,21 @@ public class ReminderUtils {
         calendar.setTimeInMillis(dateTime.getValue());
         return calendar;
     }
+
+    public static Calendar getCalendar(DateTimeTimeZone dateTimeTimeZone) {
+        String pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSS";
+        DateFormat dateFormat = new SimpleDateFormat(pattern);
+        Calendar calendar = Calendar.getInstance();
+
+        try {
+            Date date = dateFormat.parse(dateTimeTimeZone.dateTime);
+            calendar.setTimeInMillis(date.getTime());
+            calendar.setTimeZone(getTimeZone(dateTimeTimeZone.timeZone));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return calendar;
+    }
+
 }

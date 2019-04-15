51c4865a5aee11f0a806ddec0366093fec31de5b
Gaurav Sahu <contact.gauravsahu@gmail.com>
2019-04-15 22:37:35 +0530

    Removed unnecessary field

--- app/src/main/java/com/gsapps/reminders/model/AnniversaryEvent.java
+++ app/src/main/java/com/gsapps/reminders/model/AnniversaryEvent.java
@@ -1,7 +1,5 @@
 package com.gsapps.reminders.model;
 
-import java.util.Date;
-
 public class AnniversaryEvent extends Event {
-    private Date anniversaryDate;
+
 }
--- app/src/main/java/com/gsapps/reminders/model/BirthdayEvent.java
+++ app/src/main/java/com/gsapps/reminders/model/BirthdayEvent.java
@@ -1,7 +1,5 @@
 package com.gsapps.reminders.model;
 
-import java.util.Date;
-
 public class BirthdayEvent extends Event {
-    private Date birthDate;
+
 }

af879cc01530cf37e2261119ea6c10b23ee1eb8e
Gaurav Sahu <contact.gauravsahu@gmail.com>
2019-06-03 22:16:20 +0530

    Added getters and setters.

--- app/src/main/java/com/gsapps/reminders/model/TravelEvent.java
+++ app/src/main/java/com/gsapps/reminders/model/TravelEvent.java
@@ -1,9 +1,22 @@
 package com.gsapps.reminders.model;
 
-import java.util.Date;
-
 public class TravelEvent extends Event {
     private String source;
     private String destination;
-    private Date travelDate;
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
 }

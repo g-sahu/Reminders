fc2b6ad72a584d31624b988dc8e4065f7db2a5b9
Gaurav Sahu <contact.gauravsahu@gmail.com>
2019-06-11 20:13:36 +0530

    Added database class for Room.

---
+++ app/src/main/java/com/gsapps/reminders/dao/RemindersDatabase.java
@@ -0,0 +1,31 @@
+package com.gsapps.reminders.dao;
+
+import android.content.Context;
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+import com.gsapps.reminders.entities.Event;
+
+import static androidx.room.Room.databaseBuilder;
+
+@Database(entities = {Event.class}, version = 1)
+public abstract class RemindersDatabase extends RoomDatabase {
+    public abstract RemindersDao getRemindersDao();
+
+    private static volatile RemindersDatabase INSTANCE;
+
+    public static RemindersDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (RemindersDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = databaseBuilder(context.getApplicationContext(),
+                                                RemindersDatabase.class,
+                                                "reminders")
+                                    .allowMainThreadQueries()
+                                    .build();
+                }
+            }
+        }
+
+        return INSTANCE;
+    }
+}

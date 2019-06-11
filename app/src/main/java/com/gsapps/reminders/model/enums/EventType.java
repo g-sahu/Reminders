d82caec2451a050305fccfd10c3ce0833a77627b
Gaurav Sahu <contact.gauravsahu@gmail.com>
2019-06-11 20:17:29 +0530

    Added codes for enum constants.

--- app/src/main/java/com/gsapps/reminders/model/enums/EventType.java
+++ app/src/main/java/com/gsapps/reminders/model/enums/EventType.java
@@ -1,5 +1,14 @@
 package com.gsapps.reminders.model.enums;
 
 public enum EventType {
-    CONTACT, HOLIDAY, MEETING, TRAVEL
+    CONTACT(1), HOLIDAY(2), MEETING(3), TRAVEL(4);
+    private int code;
+
+    EventType(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
 }

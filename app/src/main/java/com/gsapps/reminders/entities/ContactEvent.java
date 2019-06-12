ed5bb33b463f377cf486cba41615cd95ce70d16b
Gaurav Sahu <contact.gauravsahu@gmail.com>
2019-06-12 21:01:04 +0530

    Added fields for create_ts and last_updt_ts

--- app/src/main/java/com/gsapps/reminders/entities/ContactEvent.java
+++ app/src/main/java/com/gsapps/reminders/entities/ContactEvent.java
@@ -6,10 +6,13 @@ import androidx.room.Entity;
 import androidx.room.ForeignKey;
 import androidx.room.PrimaryKey;
 import androidx.room.TypeConverters;
+import com.gsapps.reminders.converters.CalendarConverter;
 import com.gsapps.reminders.converters.ContactEventTypeConverter;
 import com.gsapps.reminders.model.enums.ContactEventType;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Calendar;
 
 @Entity(tableName = "contact_event",
         foreignKeys = @ForeignKey(entity = Event.class, parentColumns = "event_id", childColumns = "event_id"))
@@ -27,4 +30,13 @@ public class ContactEvent {
     @ColumnInfo(name = "contact_event_type")
     @TypeConverters(ContactEventTypeConverter.class)
     public ContactEventType contactEventType;
+
+    @NonNull
+    @ColumnInfo(name = "create_ts")
+    @TypeConverters(CalendarConverter.class)
+    public Calendar createTs;
+
+    @ColumnInfo(name = "last_updt_ts")
+    @TypeConverters(CalendarConverter.class)
+    public Calendar lastUpdateTs;
 }

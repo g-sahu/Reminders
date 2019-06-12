96ba672fbed6c8cc07cf54fbde4dd257a46c6b46
Gaurav Sahu <contact.gauravsahu@gmail.com>
2019-06-12 20:34:27 +0530

    Updated methods for inserting and fetching contact events.

--- app/src/main/java/com/gsapps/reminders/dao/RemindersDao.java
+++ app/src/main/java/com/gsapps/reminders/dao/RemindersDao.java
@@ -3,6 +3,7 @@ package com.gsapps.reminders.dao;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
+import com.gsapps.reminders.entities.ContactEvent;
 import com.gsapps.reminders.entities.Event;
 
 import java.util.List;
@@ -10,8 +11,17 @@ import java.util.List;
 @Dao
 public interface RemindersDao {
     @Insert
-    void insert(Event event);
+    long insertEvent(Event event);
+
+    @Insert
+    long insertContactEvent(ContactEvent contactEvent);
 
     @Query("SELECT * FROM event ORDER BY event_id")
-    List<Event> getAllEvents();
+    List<Event> getEvents();
+
+    @Query("SELECT * " +
+            "FROM event e " +
+            "INNER JOIN contact_event ce on e.event_id = ce.event_id " +
+            "ORDER BY e.event_id")
+    List<Event> getContactEvents();
 }

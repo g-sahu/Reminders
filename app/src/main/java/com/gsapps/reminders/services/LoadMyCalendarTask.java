c4ebc07127b5354a85a0cc19c7ee28c82e5b2804
Gaurav Sahu <contact.gauravsahu@gmail.com>
2020-05-24 09:40:42 +0530

    Some refactoring

--- app/src/main/java/com/gsapps/reminders/services/LoadMyCalendarTask.java
+++ app/src/main/java/com/gsapps/reminders/services/LoadMyCalendarTask.java
@@ -39,17 +39,8 @@ public class LoadMyCalendarTask extends AsyncTask<Void, Void, List<EventDTO>> {
     private static final String LOG_TAG = LoadMyCalendarTask.class.getSimpleName();
     private final Context context;
     private final List<EventDTO> eventDTOList = new ArrayList<>();
-
-    // Projection array. Creating indices for this array instead of doing
-    // dynamic lookups improves performance.
-    private static final String[] PROJECTION_CALENDARS = {_ID, ACCOUNT_NAME, CALENDAR_DISPLAY_NAME, OWNER_ACCOUNT};
+    private static final String[] PROJECTION_CALENDARS = {_ID, OWNER_ACCOUNT};
     private static final String[] PROJECTION_EVENTS = {_ID, TITLE, DESCRIPTION, DTSTART};
-
-    // The indices for the projection array above.
-    private static final int PROJECTION_ID_INDEX = 0;
-    private static final int PROJECTION_ACCOUNT_NAME_INDEX = 1;
-    private static final int PROJECTION_DISPLAY_NAME_INDEX = 2;
-    private static final int PROJECTION_OWNER_ACCOUNT_INDEX = 3;
 
     @Override
     protected List<EventDTO> doInBackground(Void... voids) {
@@ -61,18 +52,19 @@ public class LoadMyCalendarTask extends AsyncTask<Void, Void, List<EventDTO>> {
 
         try (Cursor cursor = contentResolver.query(CONTENT_URI, PROJECTION_CALENDARS, calendarsSelection, calendarsSelectionArgs, null)) {
             while (cursor != null && cursor.moveToNext()) {
-                long calID = cursor.getLong(PROJECTION_ID_INDEX);
-                String displayName = cursor.getString(PROJECTION_DISPLAY_NAME_INDEX);
-                String accountName = cursor.getString(PROJECTION_ACCOUNT_NAME_INDEX);
-                String ownerName = cursor.getString(PROJECTION_OWNER_ACCOUNT_INDEX);
+                long calID = cursor.getLong(0);
+                String ownerName = cursor.getString(1);
 
                 try (Cursor cursor1 = contentResolver.query(Events.CONTENT_URI, PROJECTION_EVENTS, eventsSelection, new String[]{valueOf(calID)}, null)) {
                     while (cursor1 != null && cursor1.moveToNext()) {
                         EventDTO eventDTO = createEventDTO(ownerName);
-                        eventDTO.setTitle(cursor1.getString(1));
-                        eventDTO.setEventDesc(cursor1.getString(2));
-                        eventDTO.setStartTs(getCalendar(cursor1.getLong(3)));
-                        eventDTOList.add(eventDTO);
+
+                        if (eventDTO != null) {
+                            eventDTO.setTitle(cursor1.getString(1));
+                            eventDTO.setEventDesc(cursor1.getString(2));
+                            eventDTO.setStartTs(getCalendar(cursor1.getLong(3)));
+                            eventDTOList.add(eventDTO);
+                        }
                     }
                 }
             }
@@ -83,22 +75,16 @@ public class LoadMyCalendarTask extends AsyncTask<Void, Void, List<EventDTO>> {
 
     private EventDTO createEventDTO(String ownerAccount) {
         final EventDTOFactory eventDTOFactory = getEventDTOFactory();
-        EventDTO eventDTO;
+        EventDTO eventDTO = null;
 
         switch (ownerAccount) {
             case ADDRESS_BOOK_CONTACTS:
-                //String eventType = event.getGadget().getPreferences().get("goo.contactsEventType");
                 eventDTO = eventDTOFactory.createEvent(CONTACT);
                 break;
 
             case HOLIDAY_IN:
             case HOLIDAY_US:
                 eventDTO = eventDTOFactory.createEvent(HOLIDAY);
-                break;
-
-            default:
-                eventDTO = eventDTOFactory.createEvent(CONTACT);
-                // TODO: 15-04-2019 Add logic here to determine other types of events
                 break;
         }
 

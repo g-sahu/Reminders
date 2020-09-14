6a30be760061f8c4f0bed9f4b79b60e84875b65c
Gaurav Sahu <contact.gauravsahu@gmail.com>
2020-09-14 21:28:34 +0530

    Removed parcelable implementation from EventType

--- app/src/main/java/com/gsapps/reminders/util/enums/EventType.java
+++ app/src/main/java/com/gsapps/reminders/util/enums/EventType.java
@@ -1,25 +1,10 @@
 package com.gsapps.reminders.util.enums;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
-import com.gsapps.reminders.creators.EventTypeCreator;
-
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor @Getter
-public enum EventType implements Parcelable {
+public enum EventType {
     CONTACT_EVENT(1), HOLIDAY_EVENT(2), MEETING_EVENT(3), TRAVEL_EVENT(4);
     private final int code;
-    public static final Creator<EventType> CREATOR = new EventTypeCreator();
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {dest.writeInt(code);}
-
 }
